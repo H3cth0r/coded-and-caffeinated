c284a27e51fd473388c65ea241d3f692f217cc86
h3cth0r <hectorsucre13@gmail.com>
2025-12-20 23:41:03 -0600

    modified shell file:

--- shell.nix
+++ shell.nix
@@ -1,8 +1,41 @@
-{pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
+let
+  python = pkgs.python311;
+  pythonPackages = python.pkgs;
+  llvmPkgs = pkgs.llvmPackages_17;
+in pkgs.mkShell {
   buildInputs = [
+    # Python
+    python
+    pythonPackages.pip
+    pythonPackages.virtualenv
+    pythonPackages.numpy
+
+    # Compilers
     pkgs.gcc
+    llvmPkgs.clang
+    llvmPkgs.lld
+    llvmPkgs.llvm
+
+    # System deps
     pkgs.xorg.libX11
   ];
+
+  # This is CRITICAL for TinyGrad
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    llvmPkgs.llvm
+  ];
+
+  shellHook = ''
+    # Sanity check: LLVM must be visible
+    echo "Using llvm-config at: $(which llvm-config)"
+    llvm-config --version
+
+    # Create and activate virtualenv
+    if [ ! -d "venv" ]; then
+      virtualenv venv
+    fi
+    source venv/bin/activate
+  '';
 }
